f346d48652f79a48aa3189108b18d9a1c7a14f4b
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-07-25 16:46:25 +0000

    datatype

---
+++ Datatype.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+9)  // we are useing node js, not browser
+
+console.log(3
+    +
+    9) //code readbility should be high
+
+console.log("pawan")
+
+
+let name ="Pawan"
+let age = 18
+let isloggedIn = false
+
+// premitive Data type
+//number=> 2 to the power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null => standlone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
