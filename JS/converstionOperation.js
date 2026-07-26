bb2f05e99d07102d0fbf21735af99913275f484e
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-07-26 16:46:09 +0000

    converstion of datatype

---
+++ JS/converstionOperation.js
@@ -0,0 +1,30 @@
+let age = false
+
+// console. log(typeof age);
+// console. log(typeof(age));
+
+let valueInNumber = Number(age)
+// console. log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc"=> NaN
+//true=>1;
+//false=>0;
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false;
+//"=>false
+// "Pawan"=>true
+
+
+let someNumber = 32
+
+let stringNumber = String(someNumber)
+console. log(typeof stringNumber);
+
