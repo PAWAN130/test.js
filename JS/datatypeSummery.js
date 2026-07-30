c2e867fd3cfdc3b1a326bceb7e682f442e3eec1e
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-07-30 19:08:09 +0000

    datatypes

---
+++ JS/datatypeSummery.js
@@ -0,0 +1,40 @@
+// Primitive Data type
+
+
+const score = 100
+const scoreValue = 100.8
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('1233')
+const anotherId = symbol('1233')
+
+console.log(id === anotherId);
+
+// const bigNumber = 222453n
+
+
+
+//7-tyoes of categary : string, Number, Boolean, null, undefined, symbol BigInt
+
+//Refrance type (non-primitive)
+
+//Array, Objects, Function
+
+const singers = ["Arijit", "atif", "arman"];
+let myObj = {
+    name: "pawan",
+    age: 23,
+
+}
+
+const myFunction = function(){
+    console.log("Hello");
+
+
+}
+console.log(typeof scoreValue)
+
+
