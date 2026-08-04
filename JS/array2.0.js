637c57ecaa17dbb7a1bb10594c35e890fa988898
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-08-04 18:32:32 +0000

    array2.0

--- JS/array2.0.js
+++ JS/array2.0.js
@@ -6,5 +6,25 @@ const dc_heros = ["Superman", "Flash", "Batman"]
 // console.log(marval_heros);
 // console.log(marval_heros[3][1]);
 
-// const allHero = marval_heros.concat(dc_heros)
+// const allHeros = marval_heros.concat(dc_heros)
 // console.log(allHeros);
+
+const all_new_heros = [...marval_heros, ...dc_heros]
+// console.log(all_new_heros);
+const array1=[2, 4,10, [12, 9],8 [11,[17,18]]]
+console.log(array1);
+
+
+
+Array.isArray("Pawan");
+
+console.log(Array.isArray("Pawan"))
+console.log(Array.from("Pawan"))
+console.log(Array.from({name: "Pawan"})) //interesting
+
+
+let score1=100
+let score2=300
+let score3=400
+
+console.log(Array.of(score1, score2, score3));
