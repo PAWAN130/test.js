884e8ad7b0709890a06c9112178dfb4d5cf2b7e0
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-08-04 08:43:20 +0000

    array2.0

---
+++ JS/array2.0.js
@@ -0,0 +1,10 @@
+const marval_heros =["Thar", "Ironman", "spiderman"]
+const dc_heros = ["Superman", "Flash", "Batman"]
+
+// marval_heros.push(dc_heros)
+
+// console.log(marval_heros);
+// console.log(marval_heros[3][1]);
+
+// const allHero = marval_heros.concat(dc_heros)
+// console.log(allHeros);
