53fa76c984aa848eed2c56cced01c3f979c3d24b
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-08-05 16:45:41 +0000

    objects

---
+++ JS/objects.js
@@ -0,0 +1,16 @@
+//Objects:The Object type represents one of JavaScript's data types. It is used to store various keyed collections and more complex entities. Objects can be created using the Object() 
+
+//objects can be decleared in two ways
+// 1. literals    2. constructor
+
+//Singleton--> singleton is same type of object which are made by the constructor
+Object.create //--> this is constructor method
+
+// object literals--> it is used to decleared the objects
+
+const JsUser = {//this {} is known as objects
+    name: "Pawan"
+
+}
+myArray = ["P", "A","W","A","N"]
+myarray[2]
