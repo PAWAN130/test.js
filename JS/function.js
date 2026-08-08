ef5435a174c5d4d3b77012a81a931b285bcf76b0
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-08-08 18:02:58 +0000

    scope

--- JS/function.js
+++ JS/function.js
@@ -1,6 +1,6 @@
 //function--> fuction() means whatever we write in the form of code taking all them in a single package is known as function
 
-function sayMyName(){
+function sayMyName() {
     console.log("P")
     console.log("A")
     console.log("w")
@@ -17,25 +17,25 @@ sayMyName()
 
 // }
 
-function addTwoNumbers(number1,number2){  
+function addTwoNumbers(number1, number2) {
 
-//    let result= number1 + number2
-//    return result
-   return number1 + number2
+    //    let result= number1 + number2
+    //    return result
+    return number1 + number2
 
 }
 
 // addTwoNumbers(4, "15")
 // addTwoNumbers(4, "b") //these are the aregument because we cal the function and pass the value 
 
-const result = addTwoNumbers(5 , 8)
+const result = addTwoNumbers(5, 8)
 
 // console.log ("Result:", result);
 
-function loginUserMessage(username){
-    if(username=== undefined){
+function loginUserMessage(username) {
+    if (username === undefined) {
         console.log("please enter a username")
-            return
+        return
     }
     return `$ {username} just logged in`
 }
@@ -43,24 +43,24 @@ function loginUserMessage(username){
 // console.log(loginUserMessage("Pawan"))
 // console.log(loginUserMessage())
 
-function calculateCartPrice(...num1){
+function calculateCartPrice(...num1) {
     return num1
 }
 
 console.log(calculateCartPrice(100, 600, 678))
 
-function calculateCartPrice(val1, val2, ...num1){
+function calculateCartPrice(val1, val2, ...num1) {
     return num1
 }
 
 // console.log(calculateCartPrice(100, 600, 1234, 678)) // here val1 and val2 takes the value of array 100 and 600
 
-const user ={
+const user = {
     username: "Pawan",
     prices: 299
 }
 
-function handleObject(anyObject){
+function handleObject(anyObject) {
     console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
 
 }
@@ -72,7 +72,7 @@ handleObject({
 })
 
 const myNewArray = [200, 760, 440, 320, 125]
-function returnSecondValue(getArray){
+function returnSecondValue(getArray) {
     return getArray[1]
 
 }
