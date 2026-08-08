a68a9e9fec21cb30e9f4d477140d5c3109da4e9f
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-08-08 16:23:13 +0000

    function overview

--- JS/function.js
+++ JS/function.js
@@ -49,4 +49,32 @@ function calculateCartPrice(...num1){
 
 console.log(calculateCartPrice(100, 600, 678))
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
 
+// console.log(calculateCartPrice(100, 600, 1234, 678)) // here val1 and val2 takes the value of array 100 and 600
+
+const user ={
+    username: "Pawan",
+    prices: 299
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+
+}
+handleObject(user)
+
+handleObject({
+    username: "Rahul",
+    price: 419
+})
+
+const myNewArray = [200, 760, 440, 320, 125]
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+
+console.log(returnSecondValue(myNewArray));
