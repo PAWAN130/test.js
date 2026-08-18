ed3d2c380be039fce4e3a03898da6375923e8244
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-08-18 15:49:03 +0000

    Control and flow

--- ControlFlow/one.js
+++ ControlFlow/one.js
@@ -13,11 +13,41 @@
 
 //<, >, <=, >=, ==, ===, =!, !==
 
-const score =200
-if (score>100){
-   let power = "pawan"
-    // const power = "pawan"
-    console.log(`User power:${power}`);
-}
-console.log(`user power: ${power}`)
+// const score =200
+// if (score>100){
+//    let power = "pawan"
+//     // const power = "pawan"
+//     console.log(`User power:${power}`);
+// }
+// console.log(`user power: ${power}`)
 
+//shorthand notation
+
+// const balance = 1000
+// // if (balance>500) console.log("test")
+
+// if (balance < 500) {
+//     console.log("less then 500");
+
+// } else if (balance < 750) {
+//     console.log("less then 750");
+
+// } else if (balance < 900) {
+//     console.log("less then 750");
+// } else {
+//     console.log("less then 1250");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedFromEmail = true
+
+if (userLoggedIn && debitCard){
+    console.log("allow to buy course");
+}
+if(loggedInFromGoogle || loggedFromEmail){
+    // Here || is used for checking multiple condition also work as OR
+    console.log("user logged in");
+}
---
+++ ControlFlow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "april"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break; 
+
+    case "feb":
+        console.log("februry");
+        break;
+
+    case "march":
+        console.log("march");
+        break;
+
+    case "april":
+        console.log("april");
+        break; 
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+//break--> break is used to break the control flow 
