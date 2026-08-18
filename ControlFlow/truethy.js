fc6741d533fe1678a24c88e58d919fd6c14cb374
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-08-18 16:05:41 +0000

    more on truthy values

---
+++ ControlFlow/truethy.js
@@ -0,0 +1,28 @@
+// const  userEmail = "pawan@gamil.com"
+
+// if (userEmail){
+//     console.log("got the email");
+
+// } else{
+//     console.log("dont't have email");
+// }
+const  userEmail = ""
+
+if (userEmail){
+    console.log("got the email");
+
+} else{
+    console.log("dont't have email");
+}
+
+// []--> if we use this bracket then we get true value
+
+
+
+//falsy values
+// false, 0, -0, BigInt On, "", null, undefined, NaN
+
+// truthy values
+
+//"0" if the zero is under string then this will be a truthy value
+// "0", 'false', " "
