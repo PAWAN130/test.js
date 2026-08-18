3d0f7d5b1213e960f8f8d8ee50a8a2885beb3929
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-08-18 09:36:58 +0000

    control and flow

---
+++ ControlFlow/one.js
@@ -0,0 +1,23 @@
+//if
+// const isUserLoggedIn = true
+// const temperature = 4
+
+// if (temperature === 45) {
+//     console.log("less then 50");
+
+// } else {
+//     console.log("temperature is greater then 50");
+// }
+
+// console.log("executed");
+
+//<, >, <=, >=, ==, ===, =!, !==
+
+const score =200
+if (score>100){
+   let power = "pawan"
+    // const power = "pawan"
+    console.log(`User power:${power}`);
+}
+console.log(`user power: ${power}`)
+
