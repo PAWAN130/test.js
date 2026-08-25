0d04ae71705a5f288f3438a682d5a74687c0f01a
Pawan Vishwakarma <pawanvish0009@gmail.com>
2026-08-25 09:21:47 +0000

    do while loop

---
+++ Loops/dowhileLoop.js
@@ -0,0 +1,15 @@
+// let score=1
+// do{
+//     console.log(`score is ${score}`)
+//     score++
+
+// }while(score<=10);
+
+
+//whatif
+
+let score=11
+do{
+    console.log(`score is ${score}`);
+    score+1
+}while(score<=10);
